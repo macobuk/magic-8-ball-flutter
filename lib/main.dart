68091e4ee05bfc05626278f9cda12ce32326f976
macobuk <macobuk@gmail.com>
2022-10-03 13:37:07 -0600

    feat: add all features

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,52 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        debugShowCheckedModeBanner: false,
+        home: MagicBall(),
       ),
     );
+
+class MagicBall extends StatefulWidget {
+  const MagicBall({Key key}) : super(key: key);
+
+  @override
+  State<MagicBall> createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int answer = 1;
+
+  void result() {
+    setState(() {
+      answer = Random().nextInt(5) + 1;
+      print(result);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 3, 62, 239),
+          title: Text(
+            'Ask Me Anything',
+            style: TextStyle(fontSize: 30),
+          ),
+          centerTitle: true,
+        ),
+        body: Container(
+          color: Colors.blueGrey,
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: result,
+                child: Image.asset('images/ball$answer.png'),
+              ),
+            ),
+          ),
+        ));
+  }
+}
